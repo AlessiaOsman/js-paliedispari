698c246b3146c305f1eda09dfd5393f582889657
AlessiaOsman <alessia.osman@gmail.com>
2024-01-09 17:01:00 +0100

    added function

--- palindromi/js/script.js
+++ palindromi/js/script.js
@@ -5,6 +5,29 @@
 
 //Recupero degli elementi
 
-const formElement = document.getElementById('form')
-const inputField = document. getElementById('input')
+function checkString (text){
 
+    let reversedText = ''
+    let message = ''
+    let isPalind = false
+
+    for (let i = text.length-1; i>=0 && !isPalind; i--){
+        reversedText += text[i]
+
+    }
+
+    if (reversedText === text){
+        message += 'La parola è palindroma'
+        isPalind = true
+    } else {
+        isPalind = false
+        message += 'La parola non è palindroma'
+    }
+
+    return message
+
+    }
+
+const result = checkString('Anna')
+
+console.log(result)
