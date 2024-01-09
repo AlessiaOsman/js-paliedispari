c1f547110773166032ba2e61d2cf8dcf0b8164f1
AlessiaOsman <alessia.osman@gmail.com>
2024-01-09 18:16:57 +0100

    created prompt and random number function

---
+++ pari_dispari/js/functions.js
@@ -0,0 +1,8 @@
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+/**
+ * generate a random number from 1 to 5 (included)
+ * @returns {number} result
+ */
+function getRandomNumber (){
+    return Math.floor(Math.random()*5)+1;
+}
---
+++ pari_dispari/js/script.js
@@ -0,0 +1,18 @@
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. (con un prompt)
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// Sommiamo i due numeri
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// Dichiariamo chi ha vinto in console.
+
+// Chiedo all'utente pari o dispari
+
+const userChoice = prompt('Scegli pari o dispari?').trim()
+console.log(userChoice)
+
+// Chiedo all'utente un numero da 1 a 5
+
+const userNumber = parseInt(prompt('Scegli un numero da 1 a 5'))
+console.log(userNumber)
+
+
+
