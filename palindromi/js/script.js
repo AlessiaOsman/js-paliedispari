85d645227c4f9076f285611e9dd82d6dfcf306bf
AlessiaOsman <alessia.osman@gmail.com>
2024-01-09 15:38:22 +0100

    recupero elementi

--- palindromi/js/script.js
+++ palindromi/js/script.js
@@ -1,3 +1,10 @@
 // Chiedere all’utente di inserire una parola (con un prompt)
 // Creare una funzione per capire se la parola inserita è palindroma
 // Stampiamo il risultato in console
+
+
+//Recupero degli elementi
+
+const formElement = document.getElementById('form')
+const inputField = document. getElementById('input')
+
